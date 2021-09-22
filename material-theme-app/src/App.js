aefad7287d670177937e874e0c1d80f05d65ff01
kaleeswaran <kalesmca@gmail.com>
2021-09-22 14:30:30 +0530

    initial theme

---
+++ material-theme-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import PaperComponent from './components/paper';
+import ButtonSizes from './components/button';
+
+
+function App() {
+  return (
+    <div className="">
+      <ButtonSizes/>
+    </div>
+  );
+}
+
+export default App;
