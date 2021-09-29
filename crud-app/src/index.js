d368529c520f958d62ed1812980019ad14e14f79
kaleeswaran <kalesmca@gmail.com>
2021-09-29 16:23:31 +0530

    localstorage with redux done in material crud

--- crud-app/src/index.js
+++ crud-app/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import {defineCustomElements as myCustom} from 'mkw-card/loader'
+import {defineCustomElements} from 'mkw-data-table/loader';
+reportWebVitals();
+defineCustomElements(window);
+// myCustom(window);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +19,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
