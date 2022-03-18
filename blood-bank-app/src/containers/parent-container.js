e63e85c14852e3f573f2e5adc250392214da597a
Kaleeswaran M <kalesmca@gmail.com>
2022-03-18 16:50:13 +0530

    containers added

--- blood-bank-app/src/containers/parent-container.js
+++ blood-bank-app/src/containers/parent-container.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import BasicDateComponent from '../components/basic-date-component';
-
+import ChildComponent from './child-container';
 class ParentComponent extends React.Component {
+    
     constructor(props){
         super(props);
-        this.state = {newDate:"not yet updated"}
+        this.state = {newDate:"not yet updated", inputValue: 'ks'}
+        
     }
+    childCallables = null;
+
+    setChildCallables = (event) => {
+        this.childCallables = event;
+     }
+     onButtonClick = () => {
+        // Call the child function doSomething()
+        this.childCallables.doSomething(20);
+     }
+
+   
 
     parentMethod = (data) =>{
         console.log('data ::', data);
@@ -20,6 +33,11 @@ class ParentComponent extends React.Component {
         Basic Date pickers
         <BasicDateComponent onDateChange={this.parentMethod}/>
       </div>
+
+            <button onClick={()=>{this.onButtonClick()}}>child click</button>
+            <input value={this.state.inputValue} onChange={(e)=>{this.setState({inputValue: e.target.value})}}/>
+                <ChildComponent setCallables={this.setChildCallables} data={this.state.inputValue}/>
+
             </div>
         )
     }
